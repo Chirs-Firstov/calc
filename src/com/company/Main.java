5aff1b8754ba336a71a3b9d1e3777f2323351719
Evgenii <e.makarov035@gmail.com>
2022-03-13 20:29:15 +0300

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,30 @@ package com.company;
 
 import java.util.Scanner;
 
-// calc 1.0
+// calc 1.1
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Введите первое число: ");
-        int num1 = in.nextInt();
+        int num1 = scanner.nextInt();
         System.out.print("Введите второе число: ");
-        int num2 = in.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.print("Введите знак: ");
+        String sing = scanner.next();
+        switch (sing) {
+            case "+" -> {
+                int sum = num1 + num2;
+                System.out.println("Сумма = " + sum);
+            }
+            case "-" -> {
+                int sub = num1 - num2;
+                System.out.println("Разность = " + sub);
+            }
 
-        int sum = num1+num2;
 
-        System.out.println("Сумма = " + sum);
-        in.close();
+        }
+
+        scanner.close();
     }
 }
